af1c63c40b6c9d60a9cc47ffff7c080433b51200
makaroniletus1783 <daoyeyoushu@gmail.com>
2021-08-20 17:11:45 +0000

    [start]アプリケーションを作ろう２

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '15dcdc6d77677cbd42772d5d248f7ef6dc637d2c392b4bc9571bcc42aced0840caa8734b83b5c90f36d9e51e8a37c233fd1771f37e90296728bd695f4f58d326'
